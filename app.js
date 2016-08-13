a4de735f86e187fef79dbde1a0f45d8afa026f8f
Murilo Mothsin da Silveira <0134683@feevale.br>
2016-08-13 16:04:50 -0300

    Add CORS

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var User = require('./models/User.js');
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
