6d0880847f93e82ed8a2561899f7d50a4657fc15
Murilo Mothsin da Silveira <0134683@feevale.br>
2016-08-20 13:59:12 -0300

    xalai

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization");
   // intercept OPTIONS method
   if (req.method == 'OPTIONS') {
-    res.json({type: 'success'});
+    res.sendStatus(200);
   }
   else {
     next();
